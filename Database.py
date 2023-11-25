1f1d534c7ae791440901263f9993aa51cc81569f
CastleYellow <rtschneiderow@gmail.com>
2023-11-25 20:24:52 +0100

    Create recipes Database

--- Database.py
+++ Database.py
@@ -1,2 +1,13 @@
 import sqlite3
 
+# Create and connect to the database
+conn = sqlite3.connect('C:/Users/annar/OneDrive/App_Dev/Recipe_Chooser/data/recipes.db')
+
+# Create a cursor
+c = conn.cursor()
+
+# Commit our command
+conn.commit()
+
+# Close our connection
+conn.close()
