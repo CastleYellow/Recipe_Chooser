45eb22b6eb53221439730cfceda42df5715edcf8
CastleYellow <rtschneiderow@gmail.com>
2023-11-25 20:45:31 +0100

    Created Window and added table to Database

--- Database.py
+++ Database.py
@@ -6,6 +6,21 @@ conn = sqlite3.connect('C:/Users/annar/OneDrive/App_Dev/Recipe_Chooser/data/reci
 # Create a cursor
 c = conn.cursor()
 
+# Create a table
+# c.execute(""" CREATE TABLE customers (
+#         recipe_name text,
+#         ingredient_1 text,
+#         ingredient_2 text,
+#         ingredient_3 text,
+#         ingredient_4 text,
+#         ingredient_5 text,
+#         ingredient_6 text,
+#         recipe_book text,
+#         recipe_link blob
+#     )""")
+
+
+
 # Commit our command
 conn.commit()
 
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import Database
-from tkinter import *
-import numpy
+import tkinter as tk
+from PIL import ImageTk
+import sqlite3
+from numpy import random
+
+bg_colour = "#FBE870"
+
+# window
+root = tk.Tk()
+root.title("Recipe Picker")
+
+# place app in center of the screen
+root.eval("tk::PlaceWindow . center") 
+
+# create a frame widget
+frame1 = tk.Frame(root, width=500, height=600, bg=bg_colour)
+frame2 = tk.Frame(root, bg=bg_colour)
+
+for frame in (frame1, frame2):
+    frame.grid(row=0, column=0)
+
+# run app
+root.mainloop()
